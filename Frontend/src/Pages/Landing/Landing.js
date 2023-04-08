400056b9609d00e47b966853e974c3d513fd2002
Andreis Purim <andreispurim@gmail.com>
2023-04-08 13:33:49 -0300

    Forgot to remove Container from Landing

--- Frontend/src/Pages/Landing/Landing.js
+++ Frontend/src/Pages/Landing/Landing.js
@@ -6,7 +6,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Hidden from '@mui/material/Hidden';
 import CardActionArea from '@mui/material/CardActionArea';
@@ -17,7 +16,6 @@ export default function LangingPage(props) {
     <div>
       <main style={{textAlign: 'start'}}>
         <div>
-          <Container>
             <br/>
           <Paper>
             <Grid container>
@@ -84,7 +82,6 @@ export default function LangingPage(props) {
               </Grid>
             </Grid>
            </div>
-          </Container>
         </div>
       </main>      
     </div>
