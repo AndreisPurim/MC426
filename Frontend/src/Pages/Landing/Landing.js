2adac5570a26367d11b450c9382b5b194f6903ed
Andreis Purim <andreispurim@gmail.com>
2023-04-08 11:52:56 -0300

    Basic landing page structure

--- Frontend/src/Pages/Landing/Landing.js
+++ Frontend/src/Pages/Landing/Landing.js
@@ -1,14 +1,92 @@
 import React from 'react';
 
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Hidden from '@mui/material/Hidden';
+import CardActionArea from '@mui/material/CardActionArea';
 
-export default function Landing({control, setControl, setView}){
+
+export default function LangingPage(props) {
   return (
     <div>
-      <h1>
-        Landing
-      </h1>
-      <Button onClick={()=>{setView('login')}} variant="contained">Login</Button>
+      <main style={{textAlign: 'start'}}>
+        <div>
+          <Container>
+            <br/>
+          <Paper>
+            <Grid container>
+              <Grid item md={6}>
+                <div >
+                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                    MC426
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    We need to format this page
+                  </Typography>
+                  <div>
+                    {props.control.user? null:
+                      <Button variant="contained" color="primary" onClick={()=>props.setControl({...props.control, view: 'login'})}>
+                        Login
+                      </Button>
+                    }
+                  </div> 
+                </div>
+              </Grid>
+            </Grid>
+          </Paper>
+           <br />
+           <div>
+           <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+                <CardActionArea component="a" href="/">
+                  <Card>
+                    <div>
+                      <CardContent>
+                        <Typography component="h2" variant="h5">
+                          ABC
+                        </Typography>
+                        <Typography variant="subtitle1"  style={{color: 'teal'}}>
+                          Slogan
+                        </Typography>
+                        <Typography variant="subtitle1" paragraph>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum aliquet faucibus. Donec leo libero, dignissim ut leo a, faucibus vestibulum eros. Donec consequat ullamcorper enim, euismod efficitur dui maximus a. Praesent auctor molestie tincidunt. Sed libero velit, iaculis et velit eu, ullamcorper mattis diam. Proin lorem enim, malesuada eu fringilla nec, pellentesque et lectus. Morbi lorem elit, accumsan nec sem quis, pharetra vestibulum justo. Nunc quis turpis sagittis, accumsan risus euismod, ultricies est. Duis lacinia dignissim massa eget consectetur. Phasellus et dolor convallis tortor mollis elementum. Fusce nec elit eget erat scelerisque facilisis non et ipsum
+                        </Typography>
+                       
+                      </CardContent>
+                    </div>
+                  </Card>
+                </CardActionArea>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <CardActionArea component="a" href="/">
+                  <Card>
+                    <div>
+                      <CardContent>
+                        <Typography component="h2" variant="h5">
+                          BCD
+                        </Typography>
+                        <Typography variant="subtitle1"  style={{color: 'teal'}}>
+                          Frase
+                        </Typography>
+                        <Typography variant="subtitle1" paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum aliquet faucibus. Donec leo libero, dignissim ut leo a, faucibus vestibulum eros. Donec consequat ullamcorper enim, euismod efficitur dui maximus a. Praesent auctor molestie tincidunt. Sed libero velit, iaculis et velit eu, ullamcorper mattis diam. Proin lorem enim, malesuada eu fringilla nec, pellentesque et lectus. Morbi lorem elit, accumsan nec sem quis, pharetra vestibulum justo. Nunc quis turpis sagittis, accumsan risus euismod, ultricies est. Duis lacinia dignissim massa eget consectetur. Phasellus et dolor convallis tortor mollis elementum. Fusce nec elit eget erat scelerisque facilisis non et ipsum
+                        </Typography>
+                      </CardContent>
+                    </div>
+                  </Card>
+                </CardActionArea>
+              </Grid>
+            </Grid>
+           </div>
+          </Container>
+        </div>
+      </main>      
     </div>
-  )
+  );
 }
