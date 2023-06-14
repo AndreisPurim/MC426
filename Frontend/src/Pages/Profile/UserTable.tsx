3207072c9c4dbbe163c0b95a8742ef64b0da14d4
Andreis Purim <andreispurim@gmail.com>
2023-06-14 16:03:15 -0300

    fix #69; Removing bloater in UserTable

--- Frontend/src/Pages/Profile/UserTable.tsx
+++ Frontend/src/Pages/Profile/UserTable.tsx
@@ -28,6 +28,96 @@ import Typography from '@mui/material/Typography';
 
 import FormCard from './FormCard';
 
+function UserTableSelector(props: any){
+  const changeTab = (event: any, newTab: any) => { props.setTable({ ...props.table, tab: newTab }) };
+  return(
+    <Grid item>
+      <Tabs value={props.table.tab} onChange={changeTab} indicatorColor={props.table.tab === 3 ? "secondary" : "primary"} textColor={props.table.tab === 3 ? "secondary" : "primary"}>
+        <Tab label="Public" />
+        <Tab label={"Favorites (" + props.control.user.favorites.length + ")"} />
+        <Tab label="Recent" />
+        {!props.control.user.admin ? null : <Tab label={"My Forms (" + props.control.user.created.length + ")"} />}
+      </Tabs>
+    </Grid>
+  )
+}
+
+function UserTableToolbar(props: any){
+  const changeSearch = (event: { target: { value: any; }; }) => { props.setTable({ ...props.table, search: event.target.value }) };
+  const openSelect = (event: { currentTarget: any; }) => { props.setTable({ ...props.table, anchorSelect: event.currentTarget }) }
+  const closeSelect = () => { props.setTable({ ...props.table, anchorSelect: null }) }
+  const resetColumns = () => { props.setTable({ ...props.table, selectedColumns: props.table.defaultColumns }) }
+  function changeSelectedColumns(i: number) {
+    const newSelected: any = props.table.selectedColumns
+    newSelected[i] = !newSelected[i]
+    props.setTable({ ...props.table, selectedColumns: newSelected })
+  }
+  const showResetButton = () => {
+    // Not working
+    for (let i = 0; i < props.table.selectedColumns.length; ++i) {
+      if (props.table.selectedColumns[i] !== props.table.defaultColumns[i]) {
+        return true
+      }
+    }
+    return false;
+  }
+  function textTab() {
+    switch (props.table.tab) {
+      case 1: return 'Favorites'
+      case 2: return 'Recent Forms'
+      case 3: return 'My forms'
+      default: return 'All'
+    }
+  }
+  return(
+    <Toolbar>
+      <Typography variant="h6" id="tableTitle" style={{ flex: '1 1 100%' }}>
+        {textTab()} {props.table.search !== '' ? '(Searching)' : null}
+      </Typography>
+      <Tooltip title="Search">
+        <TextField onChange={changeSearch} InputProps={{ startAdornment: (<InputAdornment position="start"><SearchIcon /></InputAdornment>) }} placeholder="Search" variant="outlined" />
+      </Tooltip>
+      <Tooltip title="Select Columns">
+        <IconButton onClick={openSelect}>
+          <TocIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu keepMounted anchorEl={props.table.anchorSelect} open={Boolean(props.table.anchorSelect)} onClose={closeSelect}>
+        {props.table.columns.map((column: { id: React.Key | null | undefined; label: any }, i: number) => (
+          <ListItem key={column.id}>
+            <FormControlLabel control={<Checkbox checked={props.table.selectedColumns[i]} onChange={() => changeSelectedColumns(i)} />} label={column.label} />
+          </ListItem>
+        ))}
+        {showResetButton() ? null :
+          <MenuItem onClick={resetColumns}>
+            Reset to Default
+          </MenuItem>
+        }
+      </Menu>
+    </Toolbar>
+  )
+}
+
+function UserTableHead(props: any){
+  return(
+    <TableHead>
+      <TableRow>
+        {props.table.columns.map((column: {
+          align: "right" | "left" | "center" | "inherit" | "justify" | undefined; id: any; minWidth: any; label: any;
+        }, i: string | number) => {
+          if (props.table.selectedColumns[i]) return (
+            <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
+              <b>{column.label}</b>
+            </TableCell>
+          )
+          else return null
+        })}
+        <TableCell />
+      </TableRow>
+    </TableHead>
+  )
+}
+
 export default function UserTable(props: { example: any; setControl: (arg0: any) => void; control: { user: { username: string | number; favorites: { indexOf: (arg0: any) => number; length: string; }; recents: any; created: { indexOf: (arg0: any) => number; length: string; }; admin: any; }; }; setExample: (arg0: any) => void; }) {
   const [table, setTable] = React.useState({
     tab: 0,
@@ -41,31 +131,10 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
     rows: [],
     favorites: 0
   } as any);
-  const changeTab = (event: any, newTab: any) => { setTable({ ...table, tab: newTab }) };
+  
   const changePage = (event: any, newPage: any) => { setTable({ ...table, page: newPage }) };
   const changePerPage = (event: { target: { value: string | number; }; }) => { setTable({ ...table, page: 0, perPage: +event.target.value }) };
-  const changeSearch = (event: { target: { value: any; }; }) => { setTable({ ...table, search: event.target.value }) };
-  const openSelect = (event: { currentTarget: any; }) => { setTable({ ...table, anchorSelect: event.currentTarget }) }
-  const closeSelect = () => { setTable({ ...table, anchorSelect: null }) }
-  const resetColumns = () => { setTable({ ...table, selectedColumns: table.defaultColumns }) }
-  function changeSelectedColumns(i: number) {
-    const newSelected: any = table.selectedColumns
-    newSelected[i] = !newSelected[i]
-    setTable({ ...table, selectedColumns: newSelected })
-  }
-  const showResetButton = () => {
-    for (let i = 0; i < table.selectedColumns.length; ++i) {
-      if (table.selectedColumns[i] !== table.defaultColumns[i]) {
-        // console.log(table.selectedColumns[i],table.defaultColumns[i])
-        // return false
-      }
-      else {
-        // console.log(table.selectedColumns[i],table.defaultColumns[i])
-        // return false
-      }
-    }
-    return true;
-  }
+
   React.useEffect(() => {
     const fetchData = async () => {
       const rows = props.example.rows;
@@ -79,7 +148,6 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
       const defaultColumns = columns.filter(function (row: { default: any; }) { return row.default })
       setTable({ ...table, columns: columns, selectedColumns: defaultColumns, defaultColumns: defaultColumns, rows: rows, favorites: favs })
       // props.setAlert({open: true, text: "Error in fetching rows", severity: "error"})
-
     }
     const fetchUser = async () => {
       props.setControl({ ...props.control, user: props.example.users[props.control.user.username] });
@@ -88,7 +156,6 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
     fetchUser();
   }, [props.example]);
   function handleFavorite(row: any) {
-    // Make que API here
     const userFavorites: any = props.control.user.favorites;
     const index = userFavorites.indexOf(row.id);
     if (index === -1) {
@@ -100,14 +167,6 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
     const newUsers = props.example.users;
     newUsers[props.control.user.username].favorites = userFavorites;
     props.setExample({ ...props.example, users: newUsers })
-  }
-  function textTab() {
-    switch (table.tab) {
-      case 1: return 'Favorites'
-      case 2: return 'Recent Forms'
-      case 3: return 'My forms'
-      default: return 'All'
-    }
   }
   function selectRows() {
     let rows: any = []
@@ -141,60 +200,16 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
     }
     return rows
   }
+  const sendExtraControl: any = {...props, table, setTable}
   return (
     <Grid item container direction="column" justifyContent="center" alignItems="center" style={{ paddingTop: '2rem' }}>
-      <Grid item>
-        <Tabs value={table.tab} onChange={changeTab} indicatorColor={table.tab === 3 ? "secondary" : "primary"} textColor={table.tab === 3 ? "secondary" : "primary"}>
-          <Tab label="Public" />
-          <Tab label={"Favorites (" + props.control.user.favorites.length + ")"} />
-          <Tab label="Recent" />
-          {!props.control.user.admin ? null : <Tab label={"My Forms (" + props.control.user.created.length + ")"} />}
-        </Tabs>
-      </Grid>
+      <UserTableSelector {...sendExtraControl}/>
       <Grid item>
         <Paper elevation={3}>
-          <Toolbar>
-            <Typography variant="h6" id="tableTitle" style={{ flex: '1 1 100%' }}>
-              {textTab()} {table.search !== '' ? '(Searching)' : null}
-            </Typography>
-            <Tooltip title="Search">
-              <TextField onChange={changeSearch} InputProps={{ startAdornment: (<InputAdornment position="start"><SearchIcon /></InputAdornment>) }} placeholder="Search" variant="outlined" />
-            </Tooltip>
-            <Tooltip title="Select Columns">
-              <IconButton onClick={openSelect}>
-                <TocIcon />
-              </IconButton>
-            </Tooltip>
-            <Menu keepMounted anchorEl={table.anchorSelect} open={Boolean(table.anchorSelect)} onClose={closeSelect}>
-              {table.columns.map((column: { id: React.Key | null | undefined; label: any }, i: number) => (
-                <ListItem key={column.id}>
-                  <FormControlLabel control={<Checkbox checked={table.selectedColumns[i]} onChange={() => changeSelectedColumns(i)} />} label={column.label} />
-                </ListItem>
-              ))}
-              {showResetButton() ? null :
-                <MenuItem onClick={resetColumns}>
-                  Reset to Default
-                </MenuItem>
-              }
-            </Menu>
-          </Toolbar>
+          <UserTableToolbar {...sendExtraControl}/>
           <TableContainer>
             <Table>
-              <TableHead>
-                <TableRow>
-                  {table.columns.map((column: {
-                    align: "right" | "left" | "center" | "inherit" | "justify" | undefined; id: any; minWidth: any; label: any;
-                  }, i: string | number) => {
-                    if (table.selectedColumns[i]) return (
-                      <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
-                        <b>{column.label}</b>
-                      </TableCell>
-                    )
-                    else return null
-                  })}
-                  <TableCell />
-                </TableRow>
-              </TableHead>
+              <UserTableHead {...sendExtraControl}/>
               <TableBody>
                 {selectRows().slice(table.page * table.perPage, table.page * table.perPage + table.perPage).map((row: { [x: string]: any }) => {
                   return (
@@ -210,24 +225,17 @@ export default function UserTable(props: { example: any; setControl: (arg0: any)
                               </TableCell>
                             )
                           }
-                          else if (column.id === 'last_updated') {
+                          else{
                             return (
                               <TableCell key={column.id} align={column.align}>
-                                {new Date(row[column.id]).toISOString().slice(0, 10)}
-                              </TableCell>
-                            )
-                          }
-                          else if (column.id === 'keywords') {
-                            return (
-                              <TableCell key={column.id} align={column.align}>
-                                {row[column.id].join(', ')}
-                              </TableCell>
-                            )
-                          }
-                          else {
-                            return (
-                              <TableCell key={column.id} align={column.align}>
-                                {row[column.id]}
+                                {
+                                column.id === 'last_updated'?
+                                  new Date(row[column.id]).toISOString().slice(0, 10)
+                                :column.id === 'keywords'?
+                                  row[column.id].join(', ')
+                                :
+                                  row[column.id]
+                                }
                               </TableCell>
                             )
                           }
