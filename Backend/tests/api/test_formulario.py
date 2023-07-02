a640bc3620b82d5197a701048be84602c0ae42b4
unknown <victor.agozzini@gmail.com>
2023-07-02 11:47:19 -0300

    Fixing backend test

--- Backend/tests/api/test_formulario.py
+++ Backend/tests/api/test_formulario.py
@@ -3,7 +3,6 @@ from tests.utils.formulario import create_formulario_valido
 from models import formulario
 
 def test_cria_formulario(client: TestClient) -> None:
-    assert (response.status_code == 200)
     body = create_formulario_valido()
     response = client.post("/formulario/", json=body)
     content = response.json()
